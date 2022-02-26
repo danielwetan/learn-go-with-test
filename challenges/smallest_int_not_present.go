76be91068169d3ee97dd0e6e4557d47b7ce13485
Daniel Saputra <danielwetan.io@gmail.com>
2022-02-26 22:56:43 +0700

    refactor: smallest int not present

--- challenges/smallest_int_not_present.go
+++ challenges/smallest_int_not_present.go
@@ -8,34 +8,59 @@ output: 6
 */
 
 func smallestIntNotPresent(arr []int) int {
-	arr = sorting(arr)
+
+	smallestNum := arr[0]
 
 	for i := 0; i < len(arr); i++ {
-		if arr[i]+1 != arr[i+1] {
-			return arr[i] + 1
+		if arr[i] < smallestNum {
+			smallestNum = arr[i]
 		}
 	}
 
-	return 0
+	return next(smallestNum, arr)
 }
 
-func sorting(arr []int) []int {
-	noSwap := true
-
-	for i := len(arr); i > 0; i-- {
-		noSwap = true
-
-		for j := 0; j < i-1; j++ {
-			if arr[j] > arr[j+1] {
-				temp := arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = temp
-				noSwap = false
-			}
+func next(target int, arr []int) int {
+	for _, num := range arr {
+		if target == num {
+			return next(target+1, arr)
 		}
-
-		if noSwap { break }
 	}
 
-	return arr
+	return target
 }
+
+
+
+// func smallestIntNotPresent(arr []int) int {
+// 	arr = sorting(arr)
+
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i]+1 != arr[i+1] {
+// 			return arr[i] + 1
+// 		}
+// 	}
+
+// 	return 0
+// }
+
+// func sorting(arr []int) []int {
+// 	noSwap := true
+
+// 	for i := len(arr); i > 0; i-- {
+// 		noSwap = true
+
+// 		for j := 0; j < i-1; j++ {
+// 			if arr[j] > arr[j+1] {
+// 				temp := arr[j]
+// 				arr[j] = arr[j+1]
+// 				arr[j+1] = temp
+// 				noSwap = false
+// 			}
+// 		}
+
+// 		if noSwap { break }
+// 	}
+
+// 	return arr
+// }
--- challenges/smallest_int_not_present_test.go
+++ challenges/smallest_int_not_present_test.go
@@ -15,4 +15,24 @@ func TestSmallestIntNotPresent(t *testing.T) {
 			t.Errorf("got %v but want %v", got, want)
 		}
 	})
+
+	t.Run("test case 2", func(t *testing.T) {
+		input := []int{-4, -7, 0, -2, -1, -3, -5}
+		got := smallestIntNotPresent(input)
+		want := -6
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
+
+	t.Run("test case 3", func(t *testing.T) {
+		input := []int{-4, -7, -3, -5, -6, -8, -9}
+		got := smallestIntNotPresent(input)
+		want := -2
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
 }
