74ef2cebff4a21f06e40201bac39347a4df91ddd
Daniel Saputra <danielwetan.io@gmail.com>
2022-04-08 09:23:22 +0700

    feat: nikita and the game

---
+++ challenges/nikita_and_the_game.go
@@ -0,0 +1,25 @@
+package challenges
+
+/*
+Misalnya kita punya sebuah game dimana Nikita punya suatu array
+
+- disetiap langkahnya nikita harus membagi array menjadi dua bagian 
+- ......
+
+
+[4,1,0,1,1,0,1]
+
+[4, 1, 0, 1, 1, 0, 1]
+[4] = [1, 0, 1, 1, 0, 1] -> berhasil
+
+[1, 0, 1, 1, 0, 1]
+[1, 0, 1] = [1, 0, 1] -> berhasil
+
+[1, 0, 1]
+[1] = [0, 1] -> berhasil
+
+[0, 1]
+
+nikita berhasil melakukan 3 langkah
+*/
+
